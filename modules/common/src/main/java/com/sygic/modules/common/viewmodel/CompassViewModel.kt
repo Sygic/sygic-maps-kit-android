d275fbe3291f950fd076974f4c527f96a16d6568
Tomas Valenta <tvalenta@sygic.com>
2018-11-14 13:52:30 +0100

    Add common module. Add Compass ViewModel. Update Sygic SDK version.

---
+++ modules/common/src/main/java/com/sygic/modules/common/viewmodel/CompassViewModel.kt
@@ -0,0 +1,51 @@
+package com.sygic.modules.common.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.*
+import com.sygic.sdk.map.Camera
+import com.sygic.sdk.map.MapAnimation
+import com.sygic.sdk.position.GeoCoordinates
+
+class CompassViewModel(
+    application: Application,
+    private val cameraModel: Camera.CameraModel
+) : AndroidViewModel(application), Camera.PositionChangedListener, DefaultLifecycleObserver {
+
+    val rotation: MutableLiveData<Float> = MutableLiveData()
+
+    init {
+        rotation.value = cameraModel.rotation
+    }
+
+    override fun onStart(owner: LifecycleOwner) {
+        cameraModel.addPositionChangedListener(this)
+    }
+
+    override fun onStop(owner: LifecycleOwner) {
+        cameraModel.removePositionChangedListener(this)
+    }
+
+    fun onClick() {
+        cameraModel.setRotation(
+            0f,
+            MapAnimation(400, MapAnimation.InterpolationCurve.AccelerateDecelerate)
+        ) //todo: const
+    }
+
+    override fun onPositionChanged(geoCenter: GeoCoordinates, zoom: Float, rotation: Float, tilt: Float) {
+        this.rotation.value = rotation
+    }
+
+    override fun onPositionChangeCompleted() {
+        // Do nothing
+    }
+
+    class ViewModelFactory(private val application: Application, private val cameraModel: Camera.CameraModel) :
+        ViewModelProvider.NewInstanceFactory() {
+
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            @Suppress("UNCHECKED_CAST")
+            return CompassViewModel(application, cameraModel) as T
+        }
+    }
+}
