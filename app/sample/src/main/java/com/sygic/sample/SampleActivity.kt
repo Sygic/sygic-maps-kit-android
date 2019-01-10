58e4ffa59fb2621fbdfdcc77b7198c32b564f434
Tomas Valenta <tvalenta@sygic.com>
2019-01-10 12:17:21 +0100

    Replace bottom menu with Navigation drawer. Add default elevation to the compass. Fix map pin default colors.

--- app/sample/src/main/java/com/sygic/sample/SampleActivity.kt
+++ app/sample/src/main/java/com/sygic/sample/SampleActivity.kt
@@ -1,36 +1,24 @@
 package com.sygic.sample
 
 import android.os.Bundle
-import android.widget.Toast
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import com.sygic.modules.browsemap.BrowseMapFragment
 import com.sygic.sdk.map.factory.DrawableFactory
 import com.sygic.ui.common.sdk.mapobject.MapMarker
 import kotlinx.android.synthetic.main.activity_sample.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
-class SampleActivity : AppCompatActivity() {
+class SampleActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sample)
-
-        navigation.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.bottom_nav_browse_map -> {
-                    return@OnNavigationItemSelectedListener true
-                }
-                R.id.bottom_nav_search -> {
-                    showNotImplementedToast()
-                    return@OnNavigationItemSelectedListener false
-                }
-                R.id.bottom_nav_navigation -> {
-                    showNotImplementedToast()
-                    return@OnNavigationItemSelectedListener false
-                }
-            }
-            false
-        })
+        setSupportActionBar(toolbar)
+        initDrawer()
 
         val markerFromBuilder = MapMarker.Builder()
             .coordinates(48.130550, 17.173795)
@@ -44,19 +32,61 @@ class SampleActivity : AppCompatActivity() {
             .build()
 
         val browseMapFragment = supportFragmentManager.findFragmentById(R.id.browseMapFragment) as BrowseMapFragment
-        browseMapFragment.addMapMarkers(listOf(
-            MapMarker(48.143489, 17.150560),
-            MapMarker(48.154687, 17.114679),
-            MapMarker(48.155028, 17.155674),
-            MapMarker(48.141797, 17.097001),
-            MapMarker(48.134756, 17.127729),
-            MapMarker(48.153943, 17.125282),
-            markerFromBuilder,
-            markerFromBuilderWithCustomIcon,
-            MapMarker(48.144921, 17.114853, DrawableFactory(R.drawable.ic_map_pin))))
+        browseMapFragment.addMapMarkers(
+            listOf(
+                MapMarker(48.143489, 17.150560),
+                MapMarker(48.154687, 17.114679),
+                MapMarker(48.155028, 17.155674),
+                MapMarker(48.141797, 17.097001),
+                MapMarker(48.134756, 17.127729),
+                MapMarker(48.153943, 17.125282),
+                markerFromBuilder,
+                markerFromBuilderWithCustomIcon,
+                MapMarker(48.144921, 17.114853, DrawableFactory(R.drawable.ic_map_pin))
+            )
+        )
     }
 
-    private fun showNotImplementedToast() {
-        Toast.makeText(this, "Not implemented yet", Toast.LENGTH_SHORT).show()
+    private fun initDrawer() {
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_browse_map -> {
+
+            }
+            R.id.nav_search -> {
+
+            }
+            R.id.nav_navigation -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
+            }
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
