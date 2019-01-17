6555c4f973fd5161d7de047b20f5f630ad901b06
Tomas Valenta <tvalenta@sygic.com>
2019-01-17 16:46:04 +0100

    Add Browse Map modes sample.

---
+++ app/samples/sample-browsemap-modes/src/main/java/com/sygic/samples/BrowseMapModesActivity.kt
@@ -0,0 +1,15 @@
+package com.sygic.samples
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BrowseMapModesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_browsemap_modes)
+
+        //todo: add modes selector, add some markers
+    }
+}
--- app/samples/src/main/java/com/sygic/samples/Samples.kt
+++ app/samples/src/main/java/com/sygic/samples/Samples.kt
@@ -1,34 +1,32 @@
 package com.sygic.samples
 
-import com.sygic.samples.activities.SamplesActivity
 import com.sygic.samples.models.Sample
 
 object Samples {
 
     val browseMapSampleList: List<Sample> = listOf(
         Sample(
-            com.sygic.samples.BrowseMapDefaultActivity::class.java, R.drawable.preview_browsemap_default,
+            BrowseMapDefaultActivity::class.java, R.drawable.preview_browsemap_default,
             R.string.browse_map_default, R.string.browse_map_default_summary
         ),
         Sample(
-            com.sygic.samples.BrowseMapFullActivity::class.java, R.drawable.preview_browsemap_full,
+            BrowseMapFullActivity::class.java, R.drawable.preview_browsemap_full,
             R.string.browse_map_full, R.string.browse_map_full_summary
         ),
-        //todo
         Sample(
-            SamplesActivity::class.java, R.drawable.sygic_logo_dark,
-            R.string.app_name, R.string.not_implemented_yet
+            BrowseMapModesActivity::class.java, R.drawable.preview_browsemap_modes,
+            R.string.browse_map_modes, R.string.browse_map_modes_summary
         ),
         Sample(
-            com.sygic.samples.BrowseMapMarkersActivity::class.java, R.drawable.preview_browsemap_markers,
+            BrowseMapMarkersActivity::class.java, R.drawable.preview_browsemap_markers,
             R.string.browse_map_markers, R.string.browse_map_markers_summary
         ),
         Sample(
-            com.sygic.samples.BrowseMapClickListenerActivity::class.java, R.drawable.preview_browsemap_click_listener,
+            BrowseMapClickListenerActivity::class.java, R.drawable.preview_browsemap_click_listener,
             R.string.browse_map_click_listener, R.string.browse_map_click_listener_summary
         ),
         Sample(
-            com.sygic.samples.BrowseMapThemesActivity::class.java, R.drawable.preview_browsemap_themes,
+            BrowseMapThemesActivity::class.java, R.drawable.preview_browsemap_themes,
             R.string.browse_map_themes, R.string.browse_map_themes_summary
         )
     )
