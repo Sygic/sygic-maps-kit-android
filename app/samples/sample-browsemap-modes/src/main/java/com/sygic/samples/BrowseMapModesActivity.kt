89acc8be21be2e7e54c41620b2dcb066f9c5d1d8
Tomas Valenta <tvalenta@sygic.com>
2019-01-21 13:46:39 +0100

    Finalize BrowseMap Modes example.

--- app/samples/sample-browsemap-modes/src/main/java/com/sygic/samples/BrowseMapModesActivity.kt
+++ app/samples/sample-browsemap-modes/src/main/java/com/sygic/samples/BrowseMapModesActivity.kt
@@ -1,7 +1,13 @@
 package com.sygic.samples
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
+import com.sygic.modules.browsemap.BrowseMapFragment
+import com.sygic.modules.common.mapinteraction.MapSelectionMode
+import com.sygic.ui.common.sdk.mapobject.MapMarker
+import kotlinx.android.synthetic.main.activity_browsemap_modes.*
 
 class BrowseMapModesActivity : AppCompatActivity() {
 
@@ -10,6 +16,75 @@ class BrowseMapModesActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_browsemap_modes)
 
-        //todo: add modes selector, add some markers
+        val browseMapFragment = supportFragmentManager.findFragmentById(R.id.browseMapFragment) as BrowseMapFragment
+        val selectionModeMenu = PopupMenu(this, selectionModeButton, Gravity.END).apply {
+            menuInflater.inflate(R.menu.selection_modes_menu, menu)
+            menu.findItem(getItemId()).let {
+                it.isChecked = true
+                setSelectionModeText(it.title)
+            }
+            setOnMenuItemClickListener { item ->
+                browseMapFragment.mapSelectionMode = getMapSelectionMode(item.itemId)
+                item.isChecked = true
+                setSelectionModeText(item.title)
+                true
+            }
+        }
+
+        selectionModeButton.setOnClickListener {
+            selectionModeMenu.show()
+        }
+
+        browseMapFragment.addMapMarkers(
+            listOf(
+                MapMarker.Builder()
+                    .coordinates(48.143489, 17.150560)
+                    .title("Marker 1")
+                    .build(),
+                MapMarker.Builder()
+                    .coordinates(48.162805, 17.101621)
+                    .title("Marker 2")
+                    .build(),
+                MapMarker.Builder()
+                    .coordinates(48.165561, 17.139550)
+                    .title("Marker 3")
+                    .build(),
+                MapMarker.Builder()
+                    .coordinates(48.155028, 17.155674)
+                    .title("Marker 4")
+                    .build(),
+                MapMarker.Builder()
+                    .coordinates(48.141797, 17.097001)
+                    .title("Marker 5")
+                    .build(),
+                MapMarker.Builder()
+                    .coordinates(48.134756, 17.127729)
+                    .title("Marker 6")
+                    .build()
+            )
+        )
+    }
+
+    private fun getItemId(): Int {
+        return when ((supportFragmentManager.findFragmentById(R.id.browseMapFragment) as BrowseMapFragment).mapSelectionMode) {
+            MapSelectionMode.NONE -> R.id.selection_mode_none
+            MapSelectionMode.MARKERS_ONLY -> R.id.selection_mode_markers_only
+            MapSelectionMode.FULL -> R.id.selection_mode_full
+            else -> R.id.selection_mode_none
+        }
+    }
+
+    @MapSelectionMode
+    private fun getMapSelectionMode(itemId: Int): Int {
+        return when (itemId) {
+            R.id.selection_mode_none -> MapSelectionMode.NONE
+            R.id.selection_mode_markers_only -> MapSelectionMode.MARKERS_ONLY
+            R.id.selection_mode_full -> MapSelectionMode.FULL
+            else -> MapSelectionMode.NONE
+        }
+    }
+
+    private fun setSelectionModeText(mode: CharSequence) {
+        selectionModeButton.text = "${getString(R.string.selection_mode_is)}: $mode"
     }
 }
