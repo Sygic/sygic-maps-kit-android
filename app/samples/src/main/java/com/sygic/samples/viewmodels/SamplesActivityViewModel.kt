e4563c50c4225faae2e2e48dcc000d4fbe0ba3d4
Tomas Valenta <tvalenta@sygic.com>
2019-02-04 12:06:20 +0100

    Add links to the GitHub UiKit components documentation.

--- app/samples/src/main/java/com/sygic/samples/viewmodels/SamplesActivityViewModel.kt
+++ app/samples/src/main/java/com/sygic/samples/viewmodels/SamplesActivityViewModel.kt
@@ -13,14 +13,17 @@ import com.sygic.ui.common.extensions.asSingleEvent
 import com.sygic.ui.common.livedata.SingleLiveEvent
 
 private const val GITHUB_SOURCE_CODE = "https://github.com/Sygic/sygic-maps-kit-android"
-private const val GITHUB_WIKI = "https://github.com/Sygic/sygic-maps-kit-android/wiki"
+private const val GITHUB_WIKI = "$GITHUB_SOURCE_CODE/wiki"
+private const val GITHUB_WIKI_COMPASS = "$GITHUB_WIKI/UiKit-Compass"
+private const val GITHUB_WIKI_POI_DETAIL = "$GITHUB_WIKI/UiKit-PoiDetail"
+private const val GITHUB_WIKI_POSITION_LOCK_FAB = "$GITHUB_WIKI/UiKit-PositionLockFab"
+private const val GITHUB_WIKI_ZOOM_CONTROLS = "$GITHUB_WIKI/UiKit-ZoomControls"
 private const val STACK_OVERFLOW = "https://stackoverflow.com/questions/tagged/android+sygic"
 
 class SamplesActivityViewModel : ViewModel(), DefaultLifecycleObserver, NavigationView.OnNavigationItemSelectedListener {
 
     val closeDrawerLayoutObservable: LiveData<Any> = SingleLiveEvent()
     val samplesListFragmentsObservable: LiveData<Fragment> = SingleLiveEvent()
-    val notImplementedToastObservable: LiveData<Any> = SingleLiveEvent()
     val openLinkInBrowserObservable: LiveData<String> = SingleLiveEvent()
 
     override fun onCreate(owner: LifecycleOwner) {
@@ -32,11 +35,10 @@ class SamplesActivityViewModel : ViewModel(), DefaultLifecycleObserver, Navigati
             R.id.nav_browse_map_module -> {
                 samplesListFragmentsObservable.asSingleEvent().value = BrowseMapSamplesListFragment()
             }
-            R.id.nav_ui_kit_compass, R.id.nav_ui_kit_position_lock_fab,
-            R.id.nav_ui_kit_zoom_controls, R.id.nav_ui_kit_poi_detail -> {
-                notImplementedToastObservable.asSingleEvent().call()
-                return false
-            }
+            R.id.nav_ui_kit_compass -> openLinkInBrowserObservable.asSingleEvent().value = GITHUB_WIKI_COMPASS
+            R.id.nav_ui_kit_poi_detail -> openLinkInBrowserObservable.asSingleEvent().value = GITHUB_WIKI_POI_DETAIL
+            R.id.nav_ui_kit_position_lock_fab -> openLinkInBrowserObservable.asSingleEvent().value = GITHUB_WIKI_POSITION_LOCK_FAB
+            R.id.nav_ui_kit_zoom_controls -> openLinkInBrowserObservable.asSingleEvent().value = GITHUB_WIKI_ZOOM_CONTROLS
             R.id.nav_source_code -> openLinkInBrowserObservable.asSingleEvent().value = GITHUB_SOURCE_CODE
             R.id.nav_wiki -> openLinkInBrowserObservable.asSingleEvent().value = GITHUB_WIKI
             R.id.nav_qa -> openLinkInBrowserObservable.asSingleEvent().value = STACK_OVERFLOW
