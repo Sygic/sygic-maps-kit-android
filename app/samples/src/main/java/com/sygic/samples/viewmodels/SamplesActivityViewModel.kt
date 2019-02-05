0f0fd633fb64f06bbbfa4f062942adf859985709
Tomas Valenta <tvalenta@sygic.com>
2019-02-05 14:33:36 +0100

    Change source code links to the GitHub Wiki.

--- app/samples/src/main/java/com/sygic/samples/viewmodels/SamplesActivityViewModel.kt
+++ app/samples/src/main/java/com/sygic/samples/viewmodels/SamplesActivityViewModel.kt
@@ -7,18 +7,16 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.google.android.material.navigation.NavigationView
+import com.sygic.samples.BuildConfig
 import com.sygic.samples.R
 import com.sygic.samples.fragments.BrowseMapSamplesListFragment
 import com.sygic.ui.common.extensions.asSingleEvent
 import com.sygic.ui.common.livedata.SingleLiveEvent
 
-private const val GITHUB_SOURCE_CODE = "https://github.com/Sygic/sygic-maps-kit-android"
-private const val GITHUB_WIKI = "$GITHUB_SOURCE_CODE/wiki"
-private const val GITHUB_WIKI_COMPASS = "$GITHUB_WIKI/UiKit-Compass"
-private const val GITHUB_WIKI_POI_DETAIL = "$GITHUB_WIKI/UiKit-PoiDetail"
-private const val GITHUB_WIKI_POSITION_LOCK_FAB = "$GITHUB_WIKI/UiKit-PositionLockFab"
-private const val GITHUB_WIKI_ZOOM_CONTROLS = "$GITHUB_WIKI/UiKit-ZoomControls"
-private const val STACK_OVERFLOW = "https://stackoverflow.com/questions/tagged/android+sygic"
+private const val GITHUB_WIKI_COMPASS = "${BuildConfig.GITHUB_WIKI}UiKit-Compass"
+private const val GITHUB_WIKI_POI_DETAIL = "${BuildConfig.GITHUB_WIKI}UiKit-PoiDetail"
+private const val GITHUB_WIKI_POSITION_LOCK_FAB = "${BuildConfig.GITHUB_WIKI}UiKit-PositionLockFab"
+private const val GITHUB_WIKI_ZOOM_CONTROLS = "${BuildConfig.GITHUB_WIKI}UiKit-ZoomControls"
 
 class SamplesActivityViewModel : ViewModel(), DefaultLifecycleObserver, NavigationView.OnNavigationItemSelectedListener {
 
@@ -39,9 +37,9 @@ class SamplesActivityViewModel : ViewModel(), DefaultLifecycleObserver, Navigati
             R.id.nav_ui_kit_poi_detail -> openLinkInBrowserObservable.asSingleEvent().value = GITHUB_WIKI_POI_DETAIL
             R.id.nav_ui_kit_position_lock_fab -> openLinkInBrowserObservable.asSingleEvent().value = GITHUB_WIKI_POSITION_LOCK_FAB
             R.id.nav_ui_kit_zoom_controls -> openLinkInBrowserObservable.asSingleEvent().value = GITHUB_WIKI_ZOOM_CONTROLS
-            R.id.nav_source_code -> openLinkInBrowserObservable.asSingleEvent().value = GITHUB_SOURCE_CODE
-            R.id.nav_wiki -> openLinkInBrowserObservable.asSingleEvent().value = GITHUB_WIKI
-            R.id.nav_qa -> openLinkInBrowserObservable.asSingleEvent().value = STACK_OVERFLOW
+            R.id.nav_source_code -> openLinkInBrowserObservable.asSingleEvent().value = BuildConfig.GITHUB_REPO
+            R.id.nav_wiki -> openLinkInBrowserObservable.asSingleEvent().value = BuildConfig.GITHUB_WIKI
+            R.id.nav_qa -> openLinkInBrowserObservable.asSingleEvent().value = BuildConfig.STACK_OVERFLOW
         }
 
         closeDrawerLayoutObservable.asSingleEvent().call()
