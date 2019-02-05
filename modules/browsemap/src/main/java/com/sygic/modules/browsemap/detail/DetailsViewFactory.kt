c9f93e4f960f00a1626f54ff5d0ccb2c7de77f48
Tomáš Valenta <cz840311@gmail.com>
2019-02-05 10:56:30 +0100

    Apply suggestions from code review
    Co-Authored-By: bio007 <bio007@users.noreply.github.com>

--- modules/browsemap/src/main/java/com/sygic/modules/browsemap/detail/DetailsViewFactory.kt
+++ modules/browsemap/src/main/java/com/sygic/modules/browsemap/detail/DetailsViewFactory.kt
@@ -19,29 +19,29 @@ abstract class DetailsViewFactory : Parcelable {
     /**
      * Called when the map wants to show a details window for a selected map point.
      *
-     * @param inflater The LayoutInflater object that can be used to inflate
+     * @param inflater The [LayoutInflater] object that can be used to inflate
      * any views in the details window,
-     * @param container This is the parent view that the details view
+     * @param container This is the parent [ViewGroup] that the details view
      * will be attached to. You should not add the view itself,
-     * this can only be used to generate the LayoutParams of the view.
-     * @param poiData Data associated with selected point / marker
+     * this can only be used to generate the [LayoutParams] of the view.
+     * @param poiData [PoiData] associated with selected point / marker.
      * which can be used to enrich the layout with information.
      *
-     * @return Return the view which will be used as an details view for selected points.
+     * @return the view which will be used as an details view for selected points.
      */
     abstract fun getDetailsView(inflater: LayoutInflater, container: ViewGroup, poiData: PoiData): View
 
     /**
      * Define the X offset for the details window.
      *
-     * @return Return offset in pixels you want to move the window
+     * @return offset in pixels you want to move the window.
      */
     open fun getXOffset() = 0f
 
     /**
      * Define the Y offset for the details window.
      *
-     * @return Return offset in pixels you want to move the window
+     * @return offset in pixels you want to move the window.
      */
     open fun getYOffset() = 0f
 }
