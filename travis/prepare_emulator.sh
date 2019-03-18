c6b4c39ce67cf07d1809cd04672f53ae60a833fc
Tomas Valenta <tvalenta@sygic.com>
2019-03-18 11:34:42 +0100

    Prepare emulator script improvements.

--- travis/prepare_emulator.sh
+++ travis/prepare_emulator.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-echo no | android create avd --force -n test -t android-${ANDROID_API_LEVEL} --abi ${ANDROID_ABI} -c 400M
-emulator -avd test -no-audio -no-window &
-android-wait-for-emulator
-adb shell input keyevent 82 &
+target="system-images;android-${ANDROID_API_LEVEL};default;${ANDROID_ABI}"
+# echo y | sdkmanager --update
+# echo y | sdkmanager --install $target
+avdmanager create avd --force -n test -k $target --device "Nexus 4" -c 2048M
+QEMU_AUDIO_DRV=none $ANDROID_HOME/emulator/emulator -avd test -no-window -memory 2048 &
 
 exit 0
