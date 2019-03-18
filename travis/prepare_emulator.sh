939b870e62149dd73a30c2a08e543c34d0c358cb
Tomas Valenta <tvalenta@sygic.com>
2019-03-18 11:05:11 +0100

    Add "prepare emulator" bash script.

---
+++ travis/prepare_emulator.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo no | android create avd --force -n test -t android-${ANDROID_API_LEVEL} --abi ${ANDROID_ABI} -c 400M
+emulator -avd test -no-audio -no-window &
+android-wait-for-emulator
+adb shell input keyevent 82 &
