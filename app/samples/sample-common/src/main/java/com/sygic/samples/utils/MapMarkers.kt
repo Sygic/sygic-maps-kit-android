adfc3a97432184f1139c24f7fddb204ae3db9f8f
Tomas Valenta <tvalenta@sygic.com>
2019-03-28 17:39:57 +0100

    After merge.

--- app/samples/sample-common/src/main/java/com/sygic/samples/utils/MapMarkers.kt
+++ app/samples/sample-common/src/main/java/com/sygic/samples/utils/MapMarkers.kt
@@ -1,36 +1,37 @@
 package com.sygic.samples.utils
 
 import com.sygic.sdk.map.`object`.MapMarker
+import com.sygic.ui.common.sdk.data.BasicData
 
 object MapMarkers {
 
-    val testMarkerOne: MapMarker = MapMarker.Builder()
-        .coordinates(48.182684, 17.094457)
-        .title("Test Marker 1")
+    val testMarkerOne: MapMarker = MapMarker
+        .from(48.182684, 17.094457)
+        .payload(BasicData("Test Marker 1"))
         .build()
 
-    val testMarkerTwo: MapMarker = MapMarker.Builder()
-        .coordinates(48.162805, 17.101621)
-        .title("Test Marker 2")
+    val testMarkerTwo: MapMarker = MapMarker
+        .from(48.162805, 17.101621)
+        .payload(BasicData("Test Marker 2"))
         .build()
 
-    val testMarkerThree: MapMarker = MapMarker.Builder()
-        .coordinates(48.165561, 17.139550)
-        .title("Test Marker 3")
+    val testMarkerThree: MapMarker = MapMarker
+        .from(48.165561, 17.139550)
+        .payload(BasicData("Test Marker 3"))
         .build()
 
-    val testMarkerFour: MapMarker = MapMarker.Builder()
-        .coordinates(48.128453, 17.118402)
-        .title("Test Marker 4")
+    val testMarkerFour: MapMarker = MapMarker
+        .from(48.128453, 17.118402)
+        .payload(BasicData("Test Marker 4"))
         .build()
 
-    val testMarkerFive: MapMarker = MapMarker.Builder()
-        .coordinates(48.141797, 17.097001)
-        .title("Test Marker 5")
+    val testMarkerFive: MapMarker = MapMarker
+        .from(48.141797, 17.097001)
+        .payload(BasicData("Test Marker 5"))
         .build()
 
-    val testMarkerSix: MapMarker = MapMarker.Builder()
-        .coordinates(48.134756, 17.127729)
-        .title("Test Marker 6")
+    val testMarkerSix: MapMarker = MapMarker
+        .from(48.134756, 17.127729)
+        .payload(BasicData("Test Marker 6"))
         .build()
 }
