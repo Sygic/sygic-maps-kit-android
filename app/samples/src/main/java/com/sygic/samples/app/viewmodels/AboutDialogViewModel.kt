025c3daf1f947c831543f897913fe15eab89249d
Tomas Valenta <tvalenta@sygic.com>
2019-04-22 16:11:18 +0200

    Remove unused AboutDialogViewModel ViewModelFactory and Bindable annotation. Remove duplicated copyright.

--- app/samples/src/main/java/com/sygic/samples/app/viewmodels/AboutDialogViewModel.kt
+++ app/samples/src/main/java/com/sygic/samples/app/viewmodels/AboutDialogViewModel.kt
@@ -26,12 +26,10 @@ package com.sygic.samples.app.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import com.sygic.samples.BuildConfig
 import com.sygic.maps.uikit.views.common.extensions.abiType
 import com.sygic.maps.uikit.views.common.extensions.versionCode
 import com.sygic.maps.uikit.views.common.extensions.versionName
+import com.sygic.samples.BuildConfig
 
 internal class AboutDialogViewModel(app: Application) : AndroidViewModel(app) {
 
@@ -39,12 +37,4 @@ internal class AboutDialogViewModel(app: Application) : AndroidViewModel(app) {
     val sampleAppVersionCode: String = app.versionCode()
     val sygicSdkVersion: String = BuildConfig.SDK_VERSION
     val abiType: String = app.abiType()
-
-    class ViewModelFactory(private val app: Application) : ViewModelProvider.NewInstanceFactory() {
-
-        override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            @Suppress("UNCHECKED_CAST")
-            return AboutDialogViewModel(app) as T
-        }
-    }
 }
