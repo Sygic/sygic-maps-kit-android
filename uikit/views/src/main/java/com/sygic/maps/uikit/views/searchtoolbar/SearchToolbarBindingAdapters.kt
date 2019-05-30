8c771bfd8f2e8d5343eb67c7c7af8d8f771dc49a
Tomas Valenta <tvalenta@sygic.com>
2019-05-30 10:34:12 +0200

    After merge.

--- uikit/views/src/main/java/com/sygic/maps/uikit/views/searchtoolbar/SearchToolbarBindingAdapters.kt
+++ uikit/views/src/main/java/com/sygic/maps/uikit/views/searchtoolbar/SearchToolbarBindingAdapters.kt
@@ -1,14 +1,18 @@
 /*
  * Copyright (c) 2019 Sygic a.s. All rights reserved.
+ *
  * This project is licensed under the MIT License.
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,7 +32,6 @@ import androidx.databinding.InverseBindingListener
 import androidx.databinding.adapters.ListenerUtil
 import androidx.databinding.adapters.TextViewBindingAdapter
 import kotlinx.android.synthetic.main.layout_search_toolbar_internal.view.*
-
 
 @BindingAdapter(
     value = ["android:beforeTextChanged", "android:onTextChanged",
@@ -82,4 +85,4 @@ fun setSearchText(toolbar: SearchToolbar, text: CharSequence?) {
 }
 
 @InverseBindingAdapter(attribute = "android:text", event = "android:textAttrChanged")
-fun getSearchText(toolbar: SearchToolbar): CharSequence = toolbar.getText()
+fun getSearchText(toolbar: SearchToolbar): CharSequence = toolbar.text
