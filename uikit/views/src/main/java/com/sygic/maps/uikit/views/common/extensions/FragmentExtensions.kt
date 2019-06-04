ad47fd4c01d725bd03afa037c2e7f73aa746f16c
Tomáš Valenta <cz840311@gmail.com>
2019-06-04 14:08:46 +0200

    Update uikit/views/src/main/java/com/sygic/maps/uikit/views/common/extensions/FragmentExtensions.kt
    Co-Authored-By: Miroslav Kacera <bio007@users.noreply.github.com>

--- uikit/views/src/main/java/com/sygic/maps/uikit/views/common/extensions/FragmentExtensions.kt
+++ uikit/views/src/main/java/com/sygic/maps/uikit/views/common/extensions/FragmentExtensions.kt
@@ -33,6 +33,7 @@ import androidx.fragment.app.Fragment
 import com.sygic.maps.uikit.views.R
 import com.sygic.maps.uikit.views.common.utils.logWarning
 
+fun Fragment.showKeyboard(view: View) = context?.showKeyboard(view)
 fun Fragment.toggleKeyboard() = context?.toggleKeyboard()
 fun Fragment.hideKeyboard() = view?.let { context?.hideKeyboard(it) }
 
