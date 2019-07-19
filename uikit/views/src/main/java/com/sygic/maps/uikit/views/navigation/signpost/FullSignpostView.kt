9e9d73db925c0f243eae5c424ef002665fcf7441
Tomas Valenta <tvalenta@sygic.com>
2019-07-19 10:02:17 +0200

    Add FullSignpostView and SimplifiedSignpostView KDoc.

--- uikit/views/src/main/java/com/sygic/maps/uikit/views/navigation/signpost/FullSignpostView.kt
+++ uikit/views/src/main/java/com/sygic/maps/uikit/views/navigation/signpost/FullSignpostView.kt
@@ -27,14 +27,18 @@ package com.sygic.maps.uikit.views.navigation.signpost
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.sygic.maps.uikit.views.R
 import com.sygic.maps.uikit.views.databinding.LayoutFullSignpostViewInternalBinding
+import com.sygic.maps.uikit.views.navigation.roadsign.RoadSignsView
 import com.sygic.maps.uikit.views.navigation.roadsign.data.RoadSignData
 
 /**
- * A [FullSignpostView] /* todo: waiting for final specification */
+ * A [FullSignpostView] can be used as an visual presentation component for the navigation Signpost or Direction. It contains
+ * __distanceTextView__, __primaryDirectionImageView__, __instructionTextView__ , [RoadSignsView], __pictogramImageView__,
+ * __secondaryDirectionTextView__ and __secondaryDirectionImageView__.
  */
 @Suppress("unused", "MemberVisibilityCanBePrivate", "CustomViewStyleable")
 open class FullSignpostView @JvmOverloads constructor(
@@ -46,46 +50,101 @@ open class FullSignpostView @JvmOverloads constructor(
 
     private val binding = LayoutFullSignpostViewInternalBinding.inflate(LayoutInflater.from(context), this, true)
 
+    /**
+     * Sets the text to be displayed as the distance.
+     *
+     * @param distanceText [String] text to be displayed
+     */
     fun setDistance(distanceText: String?) {
         binding.signpostDistanceTextView.text = distanceText
     }
 
+    /**
+     * Sets the drawable to be displayed as the primary direction.
+     *
+     * @param primaryDirectionDrawableRes [Int] primary direction drawable res to be displayed
+     */
     fun setPrimaryDirection(@DrawableRes primaryDirectionDrawableRes: Int) {
         binding.signpostPrimaryDirectionImageView.setImageResource(primaryDirectionDrawableRes)
     }
 
+    /**
+     * Register a callback to be invoked when primary direction view is clicked.
+     *
+     * @param primaryDirectionListener [View.OnClickListener] callback to invoke on primary direction click
+     */
     fun setOnPrimaryDirectionClickListener(primaryDirectionListener: OnClickListener?) {
         binding.signpostPrimaryDirectionImageView.setOnClickListener(primaryDirectionListener)
     }
 
+    /**
+     * Sets the drawable to be displayed as the secondary direction.
+     *
+     * @param secondaryDirectionDrawableRes [Int] secondary direction drawable res to be displayed
+     */
     fun setSecondaryDirection(@DrawableRes secondaryDirectionDrawableRes: Int) {
         binding.signpostSecondaryDirectionImageView.setImageResource(secondaryDirectionDrawableRes)
     }
 
+    /**
+     * Sets the secondary direction visibility.
+     *
+     * @param visible [Boolean] true to sets the secondary direction visible, false otherwise
+     */
     fun setSecondaryDirectionContainerVisible(visible: Boolean) {
         binding.signpostSecondaryDirectionContainer.visibility = if (visible) VISIBLE else GONE
     }
 
+    /**
+     * Sets the text to be displayed as the secondary direction text.
+     *
+     * @param secondaryDirectionText [Int] text to be displayed
+     */
     fun setSecondaryDirectionText(@StringRes secondaryDirectionText: Int) {
         setSecondaryDirectionText(resources.getString(secondaryDirectionText))
     }
 
+    /**
+     * Sets the text to be displayed as the secondary direction text.
+     *
+     * @param secondaryDirectionText [String] text to be displayed
+     */
     fun setSecondaryDirectionText(secondaryDirectionText: String?) {
         binding.signpostSecondaryDirectionTextView.text = secondaryDirectionText
     }
 
+    /**
+     * Sets the text to be displayed as the instruction text.
+     *
+     * @param instructionText [Int] text to be displayed
+     */
     fun setInstructionText(@StringRes instructionText: Int) {
         setInstructionText(resources.getString(instructionText))
     }
 
+    /**
+     * Sets the text to be displayed as the instruction text.
+     *
+     * @param instructionText [String] text to be displayed
+     */
     fun setInstructionText(instructionText: String?) {
         binding.signpostInstructionTextView.text = instructionText
     }
 
+    /**
+     * Sets the [RoadSignData] list to be displayed as the roadSigns icon's.
+     *
+     * @param roadSigns [List] of [RoadSignData] to be displayed
+     */
     fun setRoadSigns(roadSigns: List<RoadSignData>) {
         binding.signpostRoadSignsView.data = roadSigns
     }
 
+    /**
+     * Sets the drawable to be displayed as the pictogram icon.
+     *
+     * @param pictogramDrawableRes [Int] pictogram drawable res to be displayed
+     */
     fun setPictogram(@DrawableRes pictogramDrawableRes: Int) {
         if (pictogramDrawableRes != 0) {
             with(binding.signpostPictogramImageView) {
--- uikit/views/src/main/java/com/sygic/maps/uikit/views/navigation/signpost/SimplifiedSignpostView.kt
+++ uikit/views/src/main/java/com/sygic/maps/uikit/views/navigation/signpost/SimplifiedSignpostView.kt
@@ -27,13 +27,15 @@ package com.sygic.maps.uikit.views.navigation.signpost
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.sygic.maps.uikit.views.R
 import com.sygic.maps.uikit.views.databinding.LayoutSimplifiedSignpostViewInternalBinding
 
 /**
- * A [SimplifiedSignpostView] is the simplified alternative to the [FullSignpostView]. /* todo: waiting for final specification */
+ * A [SimplifiedSignpostView] is just the simplified version of the [FullSignpostView] with the different layout and without
+ * RoadSigns/Pictogram functionality. See [FullSignpostView] documentation for more details.
  */
 @Suppress("unused", "MemberVisibilityCanBePrivate", "CustomViewStyleable")
 open class SimplifiedSignpostView @JvmOverloads constructor(
@@ -45,43 +47,82 @@ open class SimplifiedSignpostView @JvmOverloads constructor(
 
     private val binding = LayoutSimplifiedSignpostViewInternalBinding.inflate(LayoutInflater.from(context), this, true)
 
+    /**
+     * Sets the text to be displayed as the distance.
+     *
+     * @param distanceText [String] text to be displayed
+     */
     fun setDistance(distanceText: String?) {
         binding.signpostDistanceTextView.text = distanceText
     }
 
+    /**
+     * Sets the drawable to be displayed as the primary direction.
+     *
+     * @param primaryDirectionDrawableRes [Int] primary direction drawable res to be displayed
+     */
     fun setPrimaryDirection(@DrawableRes primaryDirectionDrawableRes: Int) {
         binding.signpostPrimaryDirectionImageView.setImageResource(primaryDirectionDrawableRes)
     }
 
+    /**
+     * Register a callback to be invoked when primary direction view is clicked.
+     *
+     * @param primaryDirectionListener [View.OnClickListener] callback to invoke on primary direction click
+     */
     fun setOnPrimaryDirectionClickListener(primaryDirectionListener: OnClickListener?) {
         binding.signpostPrimaryDirectionImageView.setOnClickListener(primaryDirectionListener)
     }
 
+    /**
+     * Sets the drawable to be displayed as the secondary direction.
+     *
+     * @param secondaryDirectionDrawableRes [Int] secondary direction drawable res to be displayed
+     */
     fun setSecondaryDirection(@DrawableRes secondaryDirectionDrawableRes: Int) {
         binding.signpostSecondaryDirectionImageView.setImageResource(secondaryDirectionDrawableRes)
     }
 
+    /**
+     * Sets the secondary direction visibility.
+     *
+     * @param visible [Boolean] true to sets the secondary direction visible, false otherwise
+     */
     fun setSecondaryDirectionContainerVisible(visible: Boolean) {
         binding.signpostSecondaryDirectionContainer.visibility = if (visible) VISIBLE else GONE
     }
 
+    /**
+     * Sets the text to be displayed as the secondary direction text.
+     *
+     * @param secondaryDirectionText [Int] text to be displayed
+     */
     fun setSecondaryDirectionText(@StringRes secondaryDirectionText: Int) {
         setSecondaryDirectionText(resources.getString(secondaryDirectionText))
     }
 
-    fun setSecondaryDirectionText(secondaryDirectionText: String) {
+    /**
+     * Sets the text to be displayed as the secondary direction text.
+     *
+     * @param secondaryDirectionText [String] text to be displayed
+     */
+    fun setSecondaryDirectionText(secondaryDirectionText: String?) {
         binding.signpostSecondaryDirectionTextView.text = secondaryDirectionText
     }
 
     /**
-     * TODO
+     * Sets the text to be displayed as the instruction text.
+     *
+     * @param instructionText [Int] text to be displayed
      */
     fun setInstructionText(@StringRes instructionText: Int) {
         setInstructionText(resources.getString(instructionText))
     }
 
     /**
-     * TODO
+     * Sets the text to be displayed as the instruction text.
+     *
+     * @param instructionText [String] text to be displayed
      */
     fun setInstructionText(instructionText: String?) {
         binding.signpostInstructionTextView.text = instructionText
