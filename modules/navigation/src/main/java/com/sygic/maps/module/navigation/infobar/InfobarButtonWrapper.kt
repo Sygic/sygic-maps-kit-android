63dea05bc814ac151ab8a9489de977cd10978311
Tomáš Valenta <tvalenta@sygic.com>
2019-09-05 14:03:30 +0200

    Update modules/navigation/src/main/java/com/sygic/maps/module/navigation/infobar/InfobarButtonWrapper.kt
    Co-Authored-By: Miroslav Kacera <bio007@users.noreply.github.com>

--- modules/navigation/src/main/java/com/sygic/maps/module/navigation/infobar/InfobarButtonWrapper.kt
+++ modules/navigation/src/main/java/com/sygic/maps/module/navigation/infobar/InfobarButtonWrapper.kt
@@ -26,7 +26,7 @@ package com.sygic.maps.module.navigation.infobar
 
 import androidx.lifecycle.MutableLiveData
 
-class InfobarButtonWrapper(
+internal class InfobarButtonWrapper(
     val imageResource: MutableLiveData<Int> = MutableLiveData(0),
     val backgroundResource: MutableLiveData<Int> = MutableLiveData(0),
     val imageTintColor: MutableLiveData<Int> = MutableLiveData(0),
