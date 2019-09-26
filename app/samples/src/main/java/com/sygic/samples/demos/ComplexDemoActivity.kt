ef83bed530c64516dd97183cbb3f1f445e636f10
Tomas Valenta <tvalenta@sygic.com>
2019-09-26 16:19:02 +0200

    Add initial state of ComplexDemoActivityViewModel. Update ComplexDemoActivity with the BrowseMapFragment. Remove status and navigation bar transparency.

--- app/samples/src/main/java/com/sygic/samples/demos/ComplexDemoActivity.kt
+++ app/samples/src/main/java/com/sygic/samples/demos/ComplexDemoActivity.kt
@@ -25,18 +25,76 @@
 package com.sygic.samples.demos
 
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
+import com.sygic.maps.module.browsemap.BROWSE_MAP_FRAGMENT_TAG
+import com.sygic.maps.module.browsemap.BrowseMapFragment
+import com.sygic.maps.module.common.mapinteraction.MapSelectionMode
+import com.sygic.maps.module.common.provider.ModuleConnectionProvider
+import com.sygic.maps.uikit.viewmodels.common.sdk.skin.VehicleSkin
 import com.sygic.samples.R
 import com.sygic.samples.app.activities.CommonSampleActivity
+import com.sygic.samples.demos.viewmodels.ComplexDemoActivityViewModel
+import com.sygic.sdk.map.Camera
+import com.sygic.sdk.map.MapAnimation
+import com.sygic.sdk.map.MapCenter
+import com.sygic.sdk.map.MapCenterSettings
+
+private val SCREEN_CENTER = MapCenter(0.5f, 0.5f)
+private val MAP_CENTER_SETTINGS = MapCenterSettings(SCREEN_CENTER, SCREEN_CENTER, MapAnimation.NONE, MapAnimation.NONE)
 
 class ComplexDemoActivity : CommonSampleActivity() {
 
     override val wikiModulePath: String? = null
+
+    private lateinit var browseMapFragment: BrowseMapFragment
+    private lateinit var viewModel: ComplexDemoActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_complex_demo)
 
-        //todo
+        viewModel = ViewModelProviders.of(this).get(ComplexDemoActivityViewModel::class.java)
+
+        browseMapFragment = if (savedInstanceState == null) {
+            placeBrowseMapFragment().apply {
+                compassEnabled = true
+                compassHideIfNorthUp = false
+                zoomControlsEnabled = true
+                positionOnMapEnabled = true
+                positionLockFabEnabled = true
+                mapSelectionMode = MapSelectionMode.FULL
+                setVehicleSkin(VehicleSkin.PEDESTRIAN)
+                cameraDataModel.apply {
+                    movementMode = Camera.MovementMode.FollowGpsPositionWithAutozoom
+                    rotationMode = Camera.RotationMode.Attitude
+                    mapCenterSettings = MAP_CENTER_SETTINGS
+                }
+                //todo: custom click listener here?
+            }
+        } else {
+            supportFragmentManager.findFragmentByTag(BROWSE_MAP_FRAGMENT_TAG) as BrowseMapFragment
+        }
+
+        setFragmentModuleConnection(browseMapFragment, viewModel)
     }
+
+    // Note: You can also create this Fragment just like in other examples directly in an XML layout file, but
+    // performance or other issues may occur (https://stackoverflow.com/a/14810676/3796931).
+    private fun placeBrowseMapFragment() =
+        BrowseMapFragment().also {
+            supportFragmentManager
+                ?.beginTransaction()
+                ?.replace(R.id.fragmentContainer, it, BROWSE_MAP_FRAGMENT_TAG)
+                ?.runOnCommit {
+                    viewModel.mapDataModel = it.mapDataModel
+                    viewModel.cameraDataModel = it.cameraDataModel
+                }
+                ?.commit()
+        }
+
+    private fun setFragmentModuleConnection(
+        fragment: BrowseMapFragment,
+        moduleConnectionProvider: ModuleConnectionProvider
+    ) = fragment.setSearchConnectionProvider(moduleConnectionProvider)
 }
---
+++ app/samples/src/main/java/com/sygic/samples/demos/viewmodels/ComplexDemoActivityViewModel.kt
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2019 Sygic a.s. All rights reserved.
+ *
+ * This project is licensed under the MIT License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.sygic.samples.demos.viewmodels
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import com.sygic.maps.module.common.provider.ModuleConnectionProvider
+import com.sygic.maps.module.search.SearchFragment
+import com.sygic.maps.uikit.viewmodels.common.extensions.addMapMarker
+import com.sygic.maps.uikit.viewmodels.common.extensions.loadDetails
+import com.sygic.maps.uikit.viewmodels.common.extensions.removeAllMapMarkers
+import com.sygic.maps.uikit.views.common.utils.logInfo
+import com.sygic.sdk.map.MapRectangle
+import com.sygic.sdk.map.`object`.MapMarker
+import com.sygic.sdk.map.data.SimpleCameraDataModel
+import com.sygic.sdk.map.data.SimpleMapDataModel
+import com.sygic.sdk.position.GeoBoundingBox
+import com.sygic.sdk.position.GeoCoordinates
+import com.sygic.sdk.search.CoordinateSearchResult
+import com.sygic.sdk.search.MapSearchResult
+import com.sygic.sdk.search.Search
+import com.sygic.sdk.search.SearchResult
+import com.sygic.sdk.search.detail.DetailPoiCategory
+import com.sygic.sdk.search.detail.DetailPoiCategoryGroup
+
+private const val MARGIN = 80
+
+class ComplexDemoActivityViewModel : ViewModel(), ModuleConnectionProvider {
+
+    var mapDataModel: SimpleMapDataModel? = null
+    var cameraDataModel: SimpleCameraDataModel? = null
+
+    private val callback: ((searchResultList: List<SearchResult>) -> Unit) = { searchResultList ->
+        mapDataModel?.removeAllMapMarkers()
+
+        if (searchResultList.isNotEmpty()) {
+            if (searchResultList.isCategoryResult()) {
+                (searchResultList.first() as MapSearchResult).loadDetails(Search.SearchDetailListener { mapSearchDetail, state ->
+                    if (state == SearchResult.ResultState.Success) {
+                        when (mapSearchDetail) {
+                            is DetailPoiCategory -> mapSearchDetail.poiList.forEach { addMapMarker(it.position) }
+                            is DetailPoiCategoryGroup -> mapSearchDetail.poiList.forEach { addMapMarker(it.position) }
+                        }
+
+                        setCameraRectangle(mapSearchDetail.boundingBox)
+                    }
+                })
+            } else {
+                searchResultList.toGeoCoordinatesList().let { geoCoordinatesList ->
+                    if (geoCoordinatesList.isNotEmpty()) {
+
+                        if (geoCoordinatesList.size == 1) {
+                            addMapMarker(geoCoordinatesList.first())
+                            cameraDataModel?.position = geoCoordinatesList.first()
+                            cameraDataModel?.zoomLevel = 10F
+                        } else {
+                            val geoBoundingBox = GeoBoundingBox(geoCoordinatesList.first(), geoCoordinatesList.first())
+                            geoCoordinatesList.forEach { geoCoordinates ->
+                                addMapMarker(geoCoordinates)
+                                geoBoundingBox.union(geoCoordinates)
+                            }
+                            setCameraRectangle(geoBoundingBox)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override val fragment: Fragment
+        get() {
+            val searchFragment = SearchFragment()
+            searchFragment.searchLocation = cameraDataModel?.position
+            searchFragment.setResultCallback(callback)
+            return searchFragment
+        }
+
+    private fun addMapMarker(position: GeoCoordinates) {
+        mapDataModel?.addMapMarker(MapMarker.at(position).build())
+    }
+
+    private fun setCameraRectangle(geoBoundingBox: GeoBoundingBox) {
+        cameraDataModel?.mapRectangle = MapRectangle(geoBoundingBox, MARGIN, MARGIN, MARGIN, MARGIN)
+    }
+}
+
+private fun List<SearchResult>.toGeoCoordinatesList(): List<GeoCoordinates> {
+    val geoCoordinatesList = mutableListOf<GeoCoordinates>()
+    forEach { searchResult ->
+        when (searchResult) {
+            is CoordinateSearchResult -> geoCoordinatesList.add(searchResult.position)
+            is MapSearchResult -> geoCoordinatesList.add(searchResult.position)
+            else -> logInfo("${searchResult.javaClass.simpleName} class conversion is not implemented yet.")
+        }
+    }
+    return geoCoordinatesList
+}
+
+private fun List<SearchResult>.isCategoryResult(): Boolean {
+    val firstSearchResult = first()
+    return size == 1 && firstSearchResult is MapSearchResult
+            && (firstSearchResult.dataType == MapSearchResult.DataType.PoiCategoryGroup
+            || firstSearchResult.dataType == MapSearchResult.DataType.PoiCategory)
+}
