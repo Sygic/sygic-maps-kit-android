86bd75721fe07103c8b71e7195e771aa640cca60
Vladimír Bielený <vbieleny@sygic.com>
2020-07-09 12:23:05 +0200

    Add fragment displaying regions

---
+++ modules/offlinemaps/src/main/java/com/sygic/maps/offlinemaps/regions/RegionsFragment.kt
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2020 Sygic a.s. All rights reserved.
+ *
+ * This project is licensed under the MIT License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.sygic.maps.offlinemaps.regions
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.sygic.maps.offlinemaps.adapter.RegionListAdapter
+import com.sygic.maps.offlinemaps.adapter.viewholder.CountryEntryViewHolder
+import com.sygic.maps.offlinemaps.base.NavigationFragment
+import com.sygic.maps.offlinemaps.databinding.FragmentRegionsBinding
+import kotlinx.android.synthetic.main.fragment_regions.*
+
+class RegionsFragment : NavigationFragment<RegionsViewModel>() {
+    override val viewModel by viewModels<RegionsViewModel>()
+
+    private val regionsAdapter: RegionListAdapter
+        get() = regionList.adapter as RegionListAdapter
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = FragmentRegionsBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        binding.adapter = RegionListAdapter()
+        binding.viewModel = viewModel
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initArguments(requireArguments())
+
+        val countryViewHolder = CountryEntryViewHolder(countryLayout, false)
+        countryViewHolder.setPrimaryActionClickListener {
+            viewModel.onCountryPrimaryActionClicked()
+        }
+
+        viewModel.countryObservable.observe(viewLifecycleOwner, Observer { countries ->
+            val countryHolder = countries[viewModel.country]!!
+            viewModel.countryHolder = countryHolder
+            countryViewHolder.bind(countryHolder)
+            progressIndicator.visibility = View.GONE
+        })
+
+        viewModel.regionObservable.observe(viewLifecycleOwner, Observer { regions ->
+            viewModel.countryHolder?.let { holder ->
+                regionsAdapter.submitList(holder.country.details.regions.map {
+                    regions[it]!!
+                })
+            }
+        })
+
+        viewModel.notifyMapChangedObservable.observe(viewLifecycleOwner, Observer {
+            if (it.first == viewModel.country) {
+                countryViewHolder.updateFromStatus(it.second, 0)
+            } else {
+                regionsAdapter.updateStatus(it.first, it.second)
+            }
+        })
+
+        viewModel.mapInstallProgressObservable.observe(viewLifecycleOwner, Observer {
+            if (it.first == viewModel.country) {
+                countryViewHolder.updateProgress(it.second)
+            } else {
+                regionsAdapter.updateProgress(it.first, it.second)
+            }
+        })
+
+        viewModel.loadMaps()
+    }
+
+    private fun initArguments(arguments: Bundle) {
+        viewModel.country = arguments.getString(COUNTRY_KEY) ?: throw IllegalArgumentException("Must pass country ISO in arguments")
+        viewModel.installed = arguments.getBoolean(INSTALLED_KEY, false)
+    }
+
+    companion object {
+        const val COUNTRY_KEY = "country"
+        const val INSTALLED_KEY = "installed"
+    }
+}
---
+++ modules/offlinemaps/src/main/java/com/sygic/maps/offlinemaps/regions/RegionsViewModel.kt
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2020 Sygic a.s. All rights reserved.
+ *
+ * This project is licensed under the MIT License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.sygic.maps.offlinemaps.regions
+
+import androidx.lifecycle.Transformations
+import com.sygic.maps.offlinemaps.base.NavigationViewModel
+import com.sygic.maps.offlinemaps.extensions.RegionAdapterHandler
+import com.sygic.maps.offlinemaps.loader.CountryHolder
+import com.sygic.maps.offlinemaps.loader.MapLoaderGlobal
+import com.sygic.maps.offlinemaps.loader.RegionHolder
+
+class RegionsViewModel : NavigationViewModel(), RegionAdapterHandler {
+    var country = ""
+    var installed = false
+
+    var countryHolder: CountryHolder? = null
+
+    private val observable = if (installed) { MapLoaderGlobal.installedCountriesObservable } else { MapLoaderGlobal.countriesObservable }
+
+    val countryObservable = observable
+    val regionObservable = Transformations.switchMap(observable) {
+        if (installed) {
+            MapLoaderGlobal.installedRegionsObservable
+        } else {
+            MapLoaderGlobal.regionsObservable
+        }
+    }
+    val notifyMapChangedObservable = MapLoaderGlobal.notifyMapChangedObservable
+    val mapInstallProgressObservable = MapLoaderGlobal.mapInstallProgressObservable
+
+    fun loadMaps() = MapLoaderGlobal.launchInScope {
+        if (installed) {
+            MapLoaderGlobal.loadInstalledMaps()
+        } else {
+            MapLoaderGlobal.loadAllMaps()
+        }
+    }
+
+    fun onCountryPrimaryActionClicked() {
+        MapLoaderGlobal.launchInScope {
+            MapLoaderGlobal.handlePrimaryMapAction(MapLoaderGlobal.countriesObservable.value!![country]!!.iso)
+        }
+    }
+
+    override fun onPrimaryButtonClicked(region: RegionHolder) {
+        MapLoaderGlobal.launchInScope {
+            MapLoaderGlobal.handlePrimaryMapAction(region.iso)
+        }
+    }
+
+    override fun onRegionClicked(region: RegionHolder) {}
+    override fun onUpdateButtonClicked(region: RegionHolder) {}
+    override fun onLoadButtonClicked(region: RegionHolder) {}
+}
